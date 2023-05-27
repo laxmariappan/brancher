f3a637c2d40bb3e6121da6ffc453dc733ccf90b3
Lax Mariappan <elaxmariappan@gmail.com>
2023-05-28 02:12:37 +0530

    remove popup

--- content.js
+++ content.js
@@ -1,16 +1,3 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 const slugify = (str) =>
   str
@@ -24,6 +11,11 @@ setTimeout(() => {
   const tiketTitle = document.querySelector(
     "*[data-test-id='issue.views.issue-base.foundation.summary.heading']"
   );
+
+  let target = document.querySelector(
+    "*[data-testid='create-button-wrapper']"
+  );
+
   let ticket="";
 
   if (typeof window !== "undefined") {
@@ -38,26 +30,23 @@ setTimeout(() => {
       if ( url.indexOf("selectedIssue=") > -1) {
         ticket =  url.split("selectedIssue=")[1];
       }
+      let div = document.createElement("div");
+      div.id = "brancher-content";
+      target.appendChild(div);
     }
 
     if (tiketTitle) {
       title = `ticket/${ticket}-${slugify(tiketTitle.textContent)}`;
-      console.log(title);
-       chrome.runtime.sendMessage(
-         {
-           data: `${title}`,
-         },
-         function (response) {
-           //console.dir(response);
-         }
-       );
+      div.innerHTML=title;
       
     } else {
       console.log("Ticket not found");
     }
   }
+
   
 },3000);
+
 
 
 
